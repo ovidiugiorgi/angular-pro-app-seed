e84f5d428dffea0aa1b2d92742763cbcbccff184
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2018-02-24 19:20:57 +0200

    Integrate AuthService with AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,14 +32,3 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
-
-/*
-var config = {
-  apiKey: "AIzaSyDm1sHvvJTyMfz8JTDSfEXfYhFV4LN7P0U",
-  authDomain: "fitness-app-7137b.firebaseapp.com",
-  databaseURL: "https://fitness-app-7137b.firebaseio.com",
-  projectId: "fitness-app-7137b",
-  storageBucket: "fitness-app-7137b.appspot.com",
-  messagingSenderId: "497298002370"
-};
-*/
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
 export const ROUTES: Routes = [
   {
     path: 'auth',
@@ -11,12 +19,25 @@ export const ROUTES: Routes = [
       { path: 'register', loadChildren: './register/register.module#RegisterModule' }
     ]
   }
-]
+];
+
+export const firebaseConfig: FirebaseAppConfig = { 
+  apiKey: "AIzaSyDm1sHvvJTyMfz8JTDSfEXfYhFV4LN7P0U",
+  authDomain: "fitness-app-7137b.firebaseapp.com",
+  databaseURL: "https://fitness-app-7137b.firebaseio.com",
+  projectId: "fitness-app-7137b",
+  storageBucket: "fitness-app-7137b.appspot.com",
+  messagingSenderId: "497298002370"
+};
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    SharedModule.forRoot()
   ]
 })
 export class AuthModule {}
