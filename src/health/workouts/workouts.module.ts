ff1ef46ad1bc141671a35c009fc88d20b6e9ec1f
Ovidiu Giorgi <ovidiu.giorgi@tremend.com>
2018-02-26 12:01:19 +0200

    Workout module transition

--- src/health/workouts/workouts.module.ts
+++ src/health/workouts/workouts.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+
+// shared modules
+import { SharedModule } from '../shared/shared.module';
+
+// components
+import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
 
 // containers
 import { WorkoutsComponent } from './containers/workouts/workouts.component';
+import { WorkoutComponent } from './containers/workout/workout.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: WorkoutsComponent }
+  { path: '', component: WorkoutsComponent },
+  { path: 'new', component: WorkoutComponent },
+  { path: ':id', component: WorkoutComponent }
 ]
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    SharedModule
   ],
   declarations: [
-    WorkoutsComponent
+    WorkoutsComponent,
+    WorkoutComponent,
+    WorkoutFormComponent
   ]
 })
 export class WorkoutsModule {}
