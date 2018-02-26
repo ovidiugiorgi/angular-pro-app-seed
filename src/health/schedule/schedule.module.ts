6458785be66a5564e194d83de25774175b58175a
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2018-02-26 13:34:39 +0200

    Setup schedule

--- src/health/schedule/schedule.module.ts
+++ src/health/schedule/schedule.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+// components
+import { ScheduleCalendarComponent } from './components/schedule-calendar/schedule-calendar.component';
+import { ScheduleDaysComponent } from './components/schedule-days/schedule-days.component';
+import { ScheduleControlsComponent } from './components/schedule-controls/schedule-controls.component';
+
 // containers
 import { ScheduleComponent } from './containers/schedule/schedule.component';
 
 export const ROUTES: Routes = [
   { path: '', component: ScheduleComponent }
-]
+];
 
 @NgModule({
   imports: [
@@ -17,7 +22,10 @@ export const ROUTES: Routes = [
     RouterModule.forChild(ROUTES)
   ],
   declarations: [
-    ScheduleComponent
+    ScheduleComponent,
+    ScheduleCalendarComponent,
+    ScheduleDaysComponent,
+    ScheduleControlsComponent
   ]
 })
 export class ScheduleModule {}
