99d13af10fde55287d794bea5f8db92322fd4b40
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2018-03-01 13:40:07 +0200

    Hook schedules into Firebase

--- src/health/shared/services/schedule/schedule.service.ts
+++ src/health/shared/services/schedule/schedule.service.ts
@@ -9,6 +9,7 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
 
 import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
 
@@ -36,6 +37,33 @@ export class ScheduleService {
 
   private date$ = new BehaviorSubject(new Date());
   private section$ = new Subject();
+  private itemList$ = new Subject();
+
+  items$ = this.itemList$
+    .withLatestFrom(this.section$)
+    .map(([ items, section ]: any[]) => {
+
+      const id = section.data.$key;
+
+      const defaults: ScheduleItem = {
+        workouts: null,
+        meals: null,
+        section: section.section,
+        timestamp: new Date(section.day).getTime()
+      };
+
+      const payload = {
+        ...(id ? section.data : defaults),
+        ...items
+      };
+
+      if (id) {
+        return this.updateSection(id, payload);
+      } else {
+        return this.createSection(payload);
+      }
+
+    });
 
   selected$ = this.section$
     .do((next: any) => this.store.set('selected', next));
@@ -84,12 +112,24 @@ export class ScheduleService {
     return this.authService.user.uid;
   }
 
+  updateItems(items: string[]) {
+    this.itemList$.next(items);
+  }
+
   updateDate(date: Date) {
     this.date$.next(date);
   }
 
   selectSection(event: any) {
     this.section$.next(event);
+  }
+
+  private createSection(payload: ScheduleItem) {
+    return this.db.list(`schedule/${this.uid}`).push(payload);
+  }
+
+  private updateSection(key: string, payload: ScheduleItem) {
+    return this.db.object(`schedule/${this.uid}/${key}`).update(payload);
   }
 
   private getSchedule(startAt: number, endAt: number) {
